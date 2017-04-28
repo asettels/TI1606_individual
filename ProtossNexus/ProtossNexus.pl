37a9b403765c1c77d1fde3e14b16ab7620fdacad
Alex Settels <a.j.settels@student.tudelft.nl>
2017-04-28 11:42:50 +0200

    First commit

---
+++ ProtossNexus/ProtossNexus.pl
@@ -0,0 +1,11 @@
+:-dynamic 
+	friendly/3,		%matches friendly/3 percept
+	resources/4, 		%matches resources/4 percept
+	train/1, 		%matches train/1 action
+	queueSize/1,		%matches queueSize/1 percept
+       	vespeneGeyser/5,
+	build/1,
+	gather/1,
+	constructionSite/3,
+	assignedAssimilator/1.
+	 
---
+++ ProtossProbe/ProtossProbe.pl
@@ -0,0 +1,12 @@
+:-dynamic self/1,		% our own ID from the self/5 percept
+	mineralField/2, 	% mineralField IDs and amounts from the mineralField/5 percept
+	vespeneGeyser/5,	% vespeneGeyser IDs and amounts from the mineralField/5 percept
+	workerActivity/1,	% our own activity state from the workerActivity/1 percept
+	gathered/1,		% used as a goal to gather mineralFields
+	build/3,
+	resources/4,
+	friendly/3,
+	constructionSite/3,
+	doing/2,
+	gatheredGas/1.
+	
